0bc470e83def740f30d22955d4ef946f609b4c22
alexm0223 <martalex24@up.edu>
2022-09-30 23:52:44 -0700

    First push

---
+++ app/src/main/java/com/example/hw215_squarespuzzle/FifteenPuzzle.java
@@ -0,0 +1,13 @@
+package com.example.hw215_squarespuzzle;
+
+/**
+ * @author Alex Martinez-Lopez
+ */
+
+public class FifteenPuzzle {
+    //The dimensions of the board(4x4)
+    private static final int DIMENSIONS = 4;
+    private int emptyCell = DIMENSIONS * DIMENSIONS;
+
+
+}
